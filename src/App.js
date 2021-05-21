8cd9ef8fc322d0bc22b4131d3cc8866c6bb78296
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2021-05-20 21:04:39 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AddUser from './Components/Users/AddUser'; 
+
+const App = () => {
+  return (
+    <div>
+      <h1>something ✅ </h1>
+      <AddUser />
+    </div>
+  );
+}
+
+export default App;
